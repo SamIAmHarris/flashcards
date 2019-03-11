5d3d1b16e731438b709b1d2a87119a80941d14a4
Sam Harris <samh@jackrabbitmobile.com>
2019-03-10 20:00:20 -0500

    Add dummy data for cards. Add in some navigation so users can get to flashcards from home screen. Add in ability to look at question/answer from cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { data } = require("../data/flashcard_data.json");
+const { cards } = data;
 
 router.get('/', (req, res) => {
-    res.render('card',
-        { prompt: "Who is buried in Grant's tomb?", hint: "Think of whose tomb it is" }
-    );
+    const nextCard = Math.floor(Math.random() * cards.length);
+    res.redirect(`/cards/${nextCard}`);
+});
+
+router.get("/:id", (req, res) => {
+    const side = req.query.side;
+    const id = req.params.id;
+    
+    if(!side || (side !== 'question' && side !== 'answer')) {
+        res.redirect(`/cards/${id}?side=question`);
+        return;
+    }
+
+    const name = req.cookies.username;
+    const text = cards[id][side];
+    const hint = side === 'question' ? cards[id].hint : undefined;
+    const sideToShow = side === 'question' ? 'answer' : 'question';
+    const sideToShowDisplay = `${sideToShow.charAt(0)}${sideToShow.slice(1)}`;
+    const templateData = { name, text, hint, id, sideToShow, sideToShowDisplay };
+    res.render("card", templateData);
 });
 
 module.exports = router;
